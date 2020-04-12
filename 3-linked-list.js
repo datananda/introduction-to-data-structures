4ba5ceb75334bbe1edf1e456cfd6537a9210b838
Madeleine Filloux <mfilloux@gmail.com>
2020-04-11 18:32:29 -0700

    refactor linked list for different storage format

--- 3-linked-list.js
+++ 3-linked-list.js
@@ -1,30 +1,19 @@
-class Node {
-    constructor(value) {
-        this._value = value;
-        this._nextIndex;
-    }
-}
+/** Class representing a Linked List */
 
 class LinkedList {
 
-    constructor() {
-      this._storage = {};
-      this._headIndex;
-      this._tailIndex;
+    constructor(value) {
+        this._storage = {value, next: null};
+        this.tail = this._storage;
     }
     /*
     * Inserts a new value to the end of the linked list
     * @param {*} value - the value to insert
     */
     insert(value) {
-        if (this._headIndex === undefined) {
-            this._storage[0] = new Node(value);
-            this._headIndex = this._tailIndex = 0;
-        } else {
-            this._storage[this._tailIndex].nextIndex = this._tailIndex + 1;
-            this._storage[this._tailIndex + 1] = new Node(value);
-            this._tailIndex++;
-        }
+        const node = {value, next: null};
+        this.tail.next = node;
+        this.tail = node;
     }
     /*
     * Deletes a node
@@ -32,37 +21,51 @@ class LinkedList {
     * @return {*} value - the deleted node's value
     */
     remove() {
-  
+
     }
     /*
     * Removes the value at the end of the linked list
     * @return {*} - the removed value
     */
     removeTail() {
+        let currentNode = this._storage;
 
+        while (currentNode.next !== this.tail) {
+            currentNode = currentNode.next;
+        }
+
+        currentNode.next = null;
+        this.tail = currentNode;
     }
     /*
     * Searches the linked list and returns true if it contains the value passed
     * @param {*} value - the value to search for
     * @return {boolean} - true if value is found, otherwise false
     */
-    contains() {
-  
+    contains(value) {
+
     }  
     /*
     * Checks if a node is the head of the linked list 
     * @param {{prev:Object|null, next:Object|null}} node - the node to check
     * @return {boolean} - true if node is the head, otherwise false
     */
-    isHead() {
-  
+    isHead(node) {
+
     }
     /*
     * Checks if a node is the tail of the linked list 
     * @param {{prev:Object|null, next:Object|null}} node - the node to check
     * @return {boolean} - true if node is the tail, otherwise false
     */
-    isTail() {
-  
+    isTail(node) {
+
     }
   }
+  
+  const myList = new LinkedList('test');
+  console.log(myList);
+
+  myList.insert('hello');
+  console.log(myList);
+  
